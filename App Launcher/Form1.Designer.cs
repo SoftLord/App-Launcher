00b09f3acc1eed97f55a737ef9647548acb6653c
Lord Alvaro <alvaro.roca.nacarino29@gmail.com>
2020-10-21 22:29:38 +0200

    Now the buttons work and open the program if installed, except the ones that are protable and the path is variable

--- App Launcher/Form1.Designer.cs
+++ App Launcher/Form1.Designer.cs
@@ -30,6 +30,10 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AppLauncher));
             this.Tittle = new System.Windows.Forms.Label();
+            this.ConchiButton = new System.Windows.Forms.Button();
+            this.WordFinderButton = new System.Windows.Forms.Button();
+            this.RaspiButton = new System.Windows.Forms.Button();
+            this.MIXButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Tittle
@@ -39,20 +43,87 @@
             this.Tittle.ForeColor = System.Drawing.Color.White;
             this.Tittle.Location = new System.Drawing.Point(0, 0);
             this.Tittle.Name = "Tittle";
-            this.Tittle.Size = new System.Drawing.Size(800, 131);
+            this.Tittle.Size = new System.Drawing.Size(984, 131);
             this.Tittle.TabIndex = 0;
             this.Tittle.Text = "/App Launcher";
             this.Tittle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             this.Tittle.Click += new System.EventHandler(this.Tittle_Click);
+            // 
+            // ConchiButton
+            // 
+            this.ConchiButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.ConchiButton.BackgroundImage = global::App_Launcher.Properties.Resources.conchi_logo;
+            this.ConchiButton.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ConchiButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ConchiButton.Location = new System.Drawing.Point(360, 399);
+            this.ConchiButton.MaximumSize = new System.Drawing.Size(250, 250);
+            this.ConchiButton.MinimumSize = new System.Drawing.Size(250, 250);
+            this.ConchiButton.Name = "ConchiButton";
+            this.ConchiButton.Size = new System.Drawing.Size(250, 250);
+            this.ConchiButton.TabIndex = 4;
+            this.ConchiButton.UseVisualStyleBackColor = true;
+            this.ConchiButton.Click += new System.EventHandler(this.ConchiButton_Click);
+            // 
+            // WordFinderButton
+            // 
+            this.WordFinderButton.BackgroundImage = global::App_Launcher.Properties.Resources.word_finder_logo;
+            this.WordFinderButton.Cursor = System.Windows.Forms.Cursors.Default;
+            this.WordFinderButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.WordFinderButton.Location = new System.Drawing.Point(360, 134);
+            this.WordFinderButton.MaximumSize = new System.Drawing.Size(250, 250);
+            this.WordFinderButton.MinimumSize = new System.Drawing.Size(250, 250);
+            this.WordFinderButton.Name = "WordFinderButton";
+            this.WordFinderButton.Size = new System.Drawing.Size(250, 250);
+            this.WordFinderButton.TabIndex = 3;
+            this.WordFinderButton.UseVisualStyleBackColor = true;
+            this.WordFinderButton.Click += new System.EventHandler(this.WordFinderButton_Click);
+            // 
+            // RaspiButton
+            // 
+            this.RaspiButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.RaspiButton.BackgroundImage = global::App_Launcher.Properties.Resources.raspiConnectionlogo;
+            this.RaspiButton.Cursor = System.Windows.Forms.Cursors.Default;
+            this.RaspiButton.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.RaspiButton.Location = new System.Drawing.Point(722, 134);
+            this.RaspiButton.MaximumSize = new System.Drawing.Size(250, 250);
+            this.RaspiButton.Name = "RaspiButton";
+            this.RaspiButton.Size = new System.Drawing.Size(250, 250);
+            this.RaspiButton.TabIndex = 2;
+            this.RaspiButton.UseVisualStyleBackColor = true;
+            this.RaspiButton.Click += new System.EventHandler(this.RaspiButton_Click);
+            // 
+            // MIXButton
+            // 
+            this.MIXButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.MIXButton.BackgroundImage = global::App_Launcher.Properties.Resources.MIX_logo;
+            this.MIXButton.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.MIXButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.MIXButton.Location = new System.Drawing.Point(12, 134);
+            this.MIXButton.MaximumSize = new System.Drawing.Size(250, 250);
+            this.MIXButton.MinimumSize = new System.Drawing.Size(250, 250);
+            this.MIXButton.Name = "MIXButton";
+            this.MIXButton.Size = new System.Drawing.Size(250, 250);
+            this.MIXButton.TabIndex = 1;
+            this.MIXButton.UseVisualStyleBackColor = true;
+            this.MIXButton.Click += new System.EventHandler(this.MIXButton_Click);
             // 
             // AppLauncher
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(984, 661);
+            this.Controls.Add(this.ConchiButton);
+            this.Controls.Add(this.WordFinderButton);
+            this.Controls.Add(this.RaspiButton);
+            this.Controls.Add(this.MIXButton);
             this.Controls.Add(this.Tittle);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1000, 700);
+            this.MinimumSize = new System.Drawing.Size(1000, 700);
             this.Name = "AppLauncher";
             this.Text = "App Launcher";
             this.ResumeLayout(false);
@@ -62,6 +133,10 @@
         #endregion
 
         private System.Windows.Forms.Label Tittle;
+        private System.Windows.Forms.Button MIXButton;
+        private System.Windows.Forms.Button RaspiButton;
+        private System.Windows.Forms.Button WordFinderButton;
+        private System.Windows.Forms.Button ConchiButton;
     }
 }
 
--- App Launcher/Form1.cs
+++ App Launcher/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +22,44 @@ namespace App_Launcher
         private void Tittle_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void MIXButton_Click(object sender, EventArgs e)
+        {
+            //check if the directory of mix exists
+            String MixPath = @"C:\Users\Public\Documents\MIX\MIX 2.0";
+            if (Directory.Exists(MixPath))
+            {
+                Process.Start(MixPath + @"\MIX.exe");
+            }
+            else
+            {
+                //sino redirigimos a la pagina de github
+                Process.Start("https://github.com/SoftLord/MIX");
+            }
+        }
+
+        private void WordFinderButton_Click(object sender, EventArgs e)
+        {
+            String WordFinderPath = @"C:\Program Files (x86)\Álvaro Roca, Inc\Word Finder";
+            if (Directory.Exists(WordFinderPath))
+            {
+                Process.Start(WordFinderPath + @"\Word Finder 1.0.exe");
+            }
+            else
+            {
+                Process.Start("https://github.com/SoftLord/Word-Finder");
+            }
+        }
+
+        private void RaspiButton_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/SoftLord/RaspiConnection");
+        }
+
+        private void ConchiButton_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/SoftLord/Conchi");
         }
     }
 }
